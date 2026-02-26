80019bc8783452bbc59a7ec20f5013e792ab906b
Kovaleva Marina <kovalevamarina516@gmail.com>
2026-02-26 13:26:26 +0400

    feat: add category model

---
+++ fastapi_app/src/schemas/categories.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+
+
+class Category(BaseModel):
+    title: str = Field(..., description='Заголовок', max_length=256)
+    description: str = Field(description='Описание')
+    slug: str = Field(..., description='Идентификатор страницы для URL')
+    is_published: bool = Field(default=True, description='Опубликовано')
+    created_at: datetime = Field(description='Добавлено')
