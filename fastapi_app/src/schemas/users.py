bbeeac135d0889716cc221cc3388766406ec17d6
Kovaleva Marina <kovalevamarina516@gmail.com>
2026-02-26 12:30:52 +0400

    feat: update user model

--- fastapi_app/src/schemas/users.py
+++ fastapi_app/src/schemas/users.py
@@ -1,6 +1,10 @@
-from pydantic import BaseModel, SecretStr, Field
+from pydantic import BaseModel, SecretStr, Field, EmailStr
 
 
 class User(BaseModel):
+    id: int
+    email: EmailStr
     login: str
+    first_name: str = Field(max_length=30)
+    last_name: str = Field(max_length=30)
     password: SecretStr
