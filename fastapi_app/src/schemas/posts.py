29f4de0b636b7791b0664eb7970d89c84e2c8026
Kovaleva Marina <kovalevamarina516@gmail.com>
2026-02-26 12:59:11 +0400

    feat: rework post model

--- fastapi_app/src/schemas/posts.py
+++ fastapi_app/src/schemas/posts.py
@@ -1,13 +1,19 @@
 from pydantic import BaseModel, Field
-
+from datetime import datetime
 from schemas.users import User
+from schemas.locations import Location
+from schemas.categories import Category
 
 
-class PostRequestSchema(BaseModel):
-    author: User
-    text: str = Field(max_length=80)
-
-
-class PostResponseSchema(BaseModel):
-    post_text: str
-    author_name: str
+class Post(BaseModel):
+    title: str = Field(..., description='Заголовок', max_length=256)
+    text: str = Field(..., description='Текст')
+    pub_date: datetime = Field(
+        description='Дата и время публикации Если установить дату и время в будущем '
+        '— можно делать отложенные публикации.')
+    is_published: bool = Field(default=True, description='Опубликовано')
+    created_at: datetime = Field(description='Добавлено')
+    author: User = Field(description='Автор публикации')
+    location: Location = Field(default=None, description='Местоположение')
+    category: Category = Field(default=None, description='Категория')
+    image: str = Field(default=None, description='URL-ссылка на изображение')
