3ef20c5f0f2532840f0a7e71df5757ec9380aab5
Kovaleva Marina <kovalevamarina516@gmail.com>
2026-03-13 00:29:08 +0400

    feat: add comment model

---
+++ fastapi_app/src/infrastructure/sqlite/models/comments.py
@@ -0,0 +1,35 @@
+from infrastructure.sqlite.database import Base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Text, DateTime, ForeignKey
+from datetime import datetime
+from .posts import Post
+from .users import User
+
+
+class Comment(Base):
+    __tablename__ = "comments"
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+        unique=True,
+        nullable=False
+    )
+    post_id: Mapped[int] = mapped_column(
+        ForeignKey("posts.id"),
+        nullable=False
+    )
+    author_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+        nullable=False
+    )
+    text: Mapped[str] = mapped_column(
+        Text,
+        nullable=False
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.now
+    )
+
+    post: Mapped["Post"] = relationship(back_populates="comments")
+    author: Mapped["User"] = relationship(back_populates="comments")
