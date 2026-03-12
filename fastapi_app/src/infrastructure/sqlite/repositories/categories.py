6d94095606211f31dee6f6508086af1437c85cce
Kovaleva Marina <kovalevamarina516@gmail.com>
2026-03-13 01:33:40 +0400

    feat: add category repository

---
+++ fastapi_app/src/infrastructure/sqlite/repositories/categories.py
@@ -0,0 +1,33 @@
+from typing import Type, List
+from sqlalchemy.orm import Session
+from src.infrastructure.sqlite.models import Category
+
+
+class CategoryRepository:
+    def __init__(self):
+        self._model: Type[Category] = Category
+
+    def get_all_categories(self, session: Session) -> List[Category]:
+        query = session.query(self._model).order_by(self._model.name)
+        return query.all()
+
+    def get_category_by_id(self, session: Session, category_id: int) -> Category:
+        query = (
+            session.query(self._model)
+            .where(self._model.id == category_id)
+        )
+        return query.scalar()
+
+    def get_published_categories(self, session: Session) -> List[Category]:
+        query = (
+            session.query(self._model)
+            .where(self._model.is_published == True)
+        )
+        return query.all()
+
+    def get_category_by_slug(self, session: Session, slug: str) -> Category:
+        query = (
+            session.query(self._model)
+            .where(self._model.slug == slug)
+        )
+        return query.scalar()
