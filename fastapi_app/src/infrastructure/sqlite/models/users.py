b4e2727daae0681ed78303774761bb2b1901aad0
Kovaleva Marina <kovalevamarina516@gmail.com>
2026-03-13 00:05:46 +0400

    feat: update user model

--- fastapi_app/src/infrastructure/sqlite/models/users.py
+++ fastapi_app/src/infrastructure/sqlite/models/users.py
@@ -1,10 +1,40 @@
 from infrastructure.sqlite.database import Base
-
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+from .posts import Post
+from .comments import Comment
 
 
 class User(Base):
     __tablename__ = "users"
 
-    login: Mapped[str] = mapped_column(primary_key=True, nullable=False, unique=True)
-    password: Mapped[str] = mapped_column(nullable=False)
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+        nullable=False,
+        unique=True
+    )
+    login: Mapped[str] = mapped_column(
+        String(30),
+        nullable=False,
+        unique=True
+    )
+    email: Mapped[str] = mapped_column(
+        String(100),
+        nullable=False,
+        unique=True
+    )
+    password: Mapped[str] = mapped_column(
+        String(100),
+        nullable=False
+    )
+    first_name: Mapped[str] = mapped_column(
+        String(30),
+        nullable=True
+    )
+    last_name: Mapped[str] = mapped_column(
+        String(30),
+        nullable=True
+    )
+
+    posts: Mapped["Post"] = relationship(back_populates="author")
+    comments: Mapped["Comment"] = relationship(back_populates="author")
