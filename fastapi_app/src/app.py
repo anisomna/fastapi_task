42dddd19b25596c3c81344a5c07c32625ba530e1
Kovaleva Marina <kovalevamarina516@gmail.com>
2026-03-26 22:50:20 +0400

    feat: add tags to routers

--- fastapi_app/src/app.py
+++ fastapi_app/src/app.py
@@ -20,10 +20,10 @@ def create_app() -> FastAPI:
     )
 
     app.include_router(base_router, prefix="/base", tags=["Base APIs"])
-    app.include_router(users_router, prefix="/users")
-    app.include_router(posts_router,prefix="/posts")
-    app.include_router(comments_router,prefix="/comments")
-    app.include_router(categories_router, prefix="/categories")
-    app.include_router(locations_router, prefix="/locations")
+    app.include_router(users_router, prefix="/users", tags=["User APIs"])
+    app.include_router(posts_router,prefix="/posts", tags=["Post APIs"])
+    app.include_router(comments_router,prefix="/comments", tags=["Comment APIs"])
+    app.include_router(categories_router, prefix="/categories", tags=["Category APIs"])
+    app.include_router(locations_router, prefix="/locations", tags=["Location APIs"])
 
     return app
